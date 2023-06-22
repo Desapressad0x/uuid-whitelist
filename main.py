fe244084316918824fdaf34348fe47612cd766ac
Desapressado <59629875+Desapressad0x@users.noreply.github.com>
2023-06-21 23:01:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 import platform
-import os
+if platform.system() != "Windows":
+  exit()
 import requests
 import time
-import subprocess
 import urllib3
+import wmi
 
 session = requests.Session()
 session.trust_env = False
 urllib3.disable_warnings()
 
-hwid = str(subprocess.check_output('wmic csproduct get uuid')).split('\\r\\n')[1].strip('\\r').strip()
-request = session.get('https://pastebin.com/raw/LINK', verify=False)
+try:
+  uuid = wmi.WMI().Win32_ComputerSystemProduct()[0].UUID
+except:
+  exit()
 
-def menu() -> None:
-  if platform.system() != "Windows":
+def checar() -> None:
+  request = session.get('https://pastebin.com/raw/LINK', verify=False)
+
+  if uuid in request.text:
+    print("Authorized HWID, redirecting...")
+    time.sleep(2.4)
+    menu()
+  else:
+    print("Unauthorized HWID")
+    time.sleep(2.4)
     exit()
-  
+
+def menu() -> None:  
   subprocess.run('cls', shell=True)
   print("hi")
 
-if hwid in request.text:
-  print("Authorized HWID, redirecting...")
-  time.sleep(2.4)
-  menu()
-else:
-  print("Unauthorized HWID")
-  time.sleep(2.4)
-  exit()
-
 if __name__ == '__main__':
-  menu()
+  checar()
