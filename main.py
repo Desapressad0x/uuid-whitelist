16190f8443276588ed135e366ecf6abd3fb4b66f
Desapressado <59629875+Desapressad0x@users.noreply.github.com>
2023-06-21 22:44:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ hwid = str(subprocess.check_output('wmic csproduct get uuid')).split('\\r\\n')[1
 request = session.get('https://pastebin.com/raw/LINK', verify=False)
 
 def menu() -> None:
-    subprocess.run('cls' if platform.system() == 'Windows' else 'clear', shell=True)
-    print("oi")
+  subprocess.run('cls' if platform.system() == 'Windows' else 'clear', shell=True)
+  print("oi")
 
 if hwid in request.text:
-   print("Authorized HWID, redirecting...")
-   time.sleep(2.4)
-   menu()
+  print("Authorized HWID, redirecting...")
+  time.sleep(2.4)
+  menu()
 else:
-   print("Unauthorized HWID")
-   time.sleep(2.4)
-   exit()
+  print("Unauthorized HWID")
+  time.sleep(2.4)
+  exit()
 
 if __name__ == '__main__':
   menu()
