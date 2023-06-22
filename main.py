d0723fe30e50c4420f405b748229f1ac3235e698
Desapressado <59629875+Desapressad0x@users.noreply.github.com>
2023-06-21 22:46:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ hwid = str(subprocess.check_output('wmic csproduct get uuid')).split('\\r\\n')[1
 request = session.get('https://pastebin.com/raw/LINK', verify=False)
 
 def menu() -> None:
-  subprocess.run('cls')
+  subprocess.run('cls', shell=True)
   print("hi")
 
 if hwid in request.text:
